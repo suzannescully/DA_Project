fbfede39ae3a193ed20d1e67d0154bbe739174db
Suzanne Scully <suzannescully96@gmail.com>
2021-04-02 11:02:44 +0100

    created an add function

---
+++ test_script.py
@@ -0,0 +1,14 @@
+# 1. Define a function to add numbers
+# 2. Pass variables into function and return answer
+# 3. Print answer to console
+
+
+def add_nums(a, b):
+    daire=a+b
+
+    return daire
+
+a = 10
+b = 4
+suzanne=add_nums(a,b)
+print(suzanne)
