10f08daa67d89a080300944f8e6c1b016bd102d4
sscully <suzannescully96@gmail.com>
2021-04-15 12:01:01 +0100

    create functions and looping

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
+
+def filter_by_col_vals(df, colName, colVal, remainingCol):
+    '''
+    this function filters a dataframe by one columns values and returns one column (remainingCol) of data
+    :param df: dataframe
+    :param colName: column name
+    :param colVal: column value
+    :param remainingCol: another column in dataframe to filter
+    :return: returns a filtered dataframe
+    '''
+
+    filtered_data = df.loc[df[colName] == colVal].filter(items=[remainingCol])
+    return filtered_data
+
+
 # 2. Import Dataset
 Emp_Attrition = pd.read_csv("Dataset/HR-Employee-Attrition.csv")
 
@@ -13,11 +28,11 @@ Emp_Attrition = pd.read_csv("Dataset/HR-Employee-Attrition.csv")
 # Looping, iterrows [1]
 # Merge dataframes [1] done
 # 4) Python
-# • Use functions to create reusable code. [1]
+# • Use functions to create reusable code. [1] done
 # • Numpy. [1]
 # • Dictionary or Lists. [1] done
 # 5) Visualize
-# • Seaborn, Matplotlib [2] bar chart with matplotlib done, scatter plot with seaborn
+# • Seaborn, Matplotlib [2] done
 
 print(Emp_Attrition.size)
 print(Emp_Attrition.columns)
@@ -39,9 +54,16 @@ print(Emp_Attrition.groupby('Department')['Age'].mean())
 # print(Emp_Attrition[['Department','Attrition']])
 df1 = Emp_Attrition.filter(items=['Department', 'Attrition'])
 
-sales_att = df1.loc[df1['Department'] == 'Sales'].filter(items=['Attrition'])
-randd_att = df1.loc[df1['Department'] == 'Research & Development'].filter(items=['Attrition'])
-hr_att = df1.loc[df1['Department'] == 'Human Resources'].filter(items=['Attrition'])
+
+
+
+sales_att = filter_by_col_vals(df1, 'Department', 'Sales', 'Attrition')
+randd_att = filter_by_col_vals(df1, 'Department', 'Research & Development', 'Attrition')
+hr_att = filter_by_col_vals(df1, 'Department', 'Human Resources', 'Attrition')
+
+# sales_att = df1.loc[df1['Department'] == 'Sales'].filter(items=['Attrition'])
+# randd_att = df1.loc[df1['Department'] == 'Research & Development'].filter(items=['Attrition'])
+# hr_att = df1.loc[df1['Department'] == 'Human Resources'].filter(items=['Attrition'])
 print(hr_att)
 
 print(sales_att.value_counts())
@@ -57,13 +79,14 @@ print(randd_att.value_counts().values)
 randd_att_vals = randd_att.value_counts().values
 
 # merge dataframes to create attrition-department matrix
-Dep_Att = pd.DataFrame(data={'No': [hr_att_vals[0], randd_att_vals[0], sales_att_vals[0]], 'Yes': [hr_att_vals[1], randd_att_vals[1], sales_att_vals[1]]}, index=['HR', 'RD', 'Sales'])
+Dep_Att = pd.DataFrame(data={'No': [hr_att_vals[0], randd_att_vals[0], sales_att_vals[0]],
+                             'Yes': [hr_att_vals[1], randd_att_vals[1], sales_att_vals[1]]},
+                       index=['HR', 'RD', 'Sales'])
 print(Dep_Att)
 
 # create bar chart showing attrition by  department
 ax = Dep_Att.plot.bar(color=['#e17055', '#81ecec'], rot=0, title='Attrition by Department', xlabel='Department')
-plt.show()
-
+#plt.show()
 
 # scatter plot age and monthly rate
 # first take the original dataframe and filter for age and monthly rate columns
@@ -72,22 +95,23 @@ df2 = Emp_Attrition.filter(items=['YearsInCurrentRole', 'MonthlyIncome'])
 print(df2)
 
 # plot mean monthly rate per job role
-#sns.scatterplot(data=df2, x='YearsInCurrentRole', y='MonthlyIncome')
-plt.show()
+# sns.scatterplot(data=df2, x='YearsInCurrentRole', y='MonthlyIncome')
+#plt.show()
 
 # seaborn barplot
 sns.barplot(data=df2, x='YearsInCurrentRole', y='MonthlyIncome')
-plt.show()
+#plt.show()
 
 # seaborn scatter
 sns.scatterplot(data=Emp_Attrition, x='EmployeeNumber', y='HourlyRate')
-plt.show()
+#plt.show()
 
 # create dataframes for male employees and female employees
 males_df = Emp_Attrition.loc[Emp_Attrition['Gender'] == 'Male']
 females_df = Emp_Attrition.loc[Emp_Attrition['Gender'] == 'Female']
 
 # create graphs comparing gender monthly income for each job role and years in current role
+fig, axs = plt.subplots(nrows=2, ncols=2)
 axs[0][0].set_title('Males')
 axs[0][1].set_title('Females')
 axs[1][0].set_title('Males')
@@ -98,23 +122,14 @@ sns.barplot(x='YearsInCurrentRole', y='MonthlyIncome', data=males_df, ax=axs[0][
 sns.barplot(x='YearsInCurrentRole', y='MonthlyIncome', data=females_df, ax=axs[0][1])
 sns.barplot(x='JobRole', y='MonthlyIncome', data=males_df.sort_values('JobRole'), ax=axs[1][0])
 sns.barplot(x='JobRole', y='MonthlyIncome', data=females_df.sort_values('JobRole'), ax=axs[1][1])
-plt.show()
+#plt.show()
+
+# filter dataframe to get monthly income vs years in company
+# group by number of years in the company and calculate mean values
+monthlyincome_yrsinrole = Emp_Attrition.filter(items=['YearsInCurrentRole', 'MonthlyIncome'])
+grouped_monthlyincome_yrsinrole = monthlyincome_yrsinrole.groupby('YearsInCurrentRole')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+my_list = [1, 2, 3, 4]
+for el in grouped_monthlyincome_yrsinrole:
+    print(el[0])
